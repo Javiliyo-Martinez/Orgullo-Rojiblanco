6fd936b106aba996dc988af764665b7469587d15
Javiliyo-Martinez <jmarrue1010@g.educaand.es>
2025-02-10 12:23:23 +0100

    Create pedirNumero.js

---
+++ pedirNumero.js
@@ -0,0 +1,9 @@
+unction pedirNumeroEnRango(min, max) {
+  var numero;
+  do {
+    numero = prompt("Ingrese un número entre " + min +" y " + max + ":");
+    numero = Number(numero); 
+  } while (numero < min || numero > max);
+
+  return numero;
+}
