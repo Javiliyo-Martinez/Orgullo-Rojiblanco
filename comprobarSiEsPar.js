853b2680ae111213087392636d5b48837048fc2e
Javiliyo-Martinez <jmarrue1010@g.educaand.es>
2025-03-04 13:15:55 +0100

    Create comprobarSiEsPar.js

---
+++ comprobarSiEsPar.js
@@ -0,0 +1,14 @@
+function comprobarSiEsPar(numero)
+  {
+    var par
+    if ( numero % 2 == 0 )
+            {
+            par = true;
+            return par;
+            }
+    else 
+            {
+            par = false;
+            return par;
+            }   
+  }
